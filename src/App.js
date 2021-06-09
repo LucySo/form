93daffffbb519332a80ed46ee8f22db98306bc32
LucieSaulnier <lucie.saulnier1988@gmail.com>
2021-06-09 13:18:53 +0200

    firstcommit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import'bootstrap/dist/css/bootstrap.min.css';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
-function App() {
+class App extends React.Component{
+  constructor(){
+    super()
+      this.state = {
+        validated: false,
+      }
+  }
+
+  handlesubmit = (event) => {
+    const form = event.currentTarget
+    if(form.checkValidity() === false) {
+      event.preventDefault()
+      event.stopPropagation()
+    }
+  
+    this.setState({validated: true})
+  }
+  render(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="p-3">
+      <h1>Login</h1>
+      <Form noValidate validated={this.state.validated} onsubmite={this.handleSubmit}>
+        <Form.Group>
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" required />
+          <Form.Control.Feedback type="invalid">
+            please provide a valid email.
+          </Form.Control.Feedback>
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password"/>
+        </Form.Group>
+        <Form.Group>
+          <Form.Check label="Remember me"/>
+        </Form.Group>
+        <Button variant="primary" type="submit">Submit
+        </Button>
+      </Form>
     </div>
   );
 }
+}
 
-export default App;
+export default App
